b19f23673e33cdd7f80730f7d180e782ad3c2270
JUNK329\kmaki <koruri821@gmail.com>
2025-10-12 14:28:36 +0900

    add: (memo-app/frontend)メモ一覧表示追加

--- 20251003_memo-app-express-next/frontend/src/App.tsx
+++ 20251003_memo-app-express-next/frontend/src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+
+
+interface Note {
+  id: number
+  content: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState<Note[]>([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/notes")
+      .then(res => res.json())
+      .then(data => setNotes(data))
+      .catch(console.error)
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ padding: 20 }}>
+      <h1>メモ一覧</h1>
+      <ul>
+        {notes.map(note => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
